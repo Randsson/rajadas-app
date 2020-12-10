24e4fa3a3d0fe610bc8df0db1cd7deee34f17b92
Randsson <randssondouglas@gmail.com>
2020-12-10 13:33:49 -0300

    add buscas

--- app/controllers/conta/display_controller.rb
+++ app/controllers/conta/display_controller.rb
@@ -6,6 +6,7 @@ class Conta::DisplayController < ApplicationController
     @cod_leitura = params[:format]
     @formas_pagamentos = { '1' => 'Dinheiro', '2' => 'Cheque', '3' => 'Nao identificado' }
     canais_de_recebimento
+    localidades
   end
 
   private
@@ -22,6 +23,99 @@ class Conta::DisplayController < ApplicationController
       '4' => 'Outros meios com fatura/guia de arrecadação',
       '5' => 'Casas lotéricas/correspondentes bancários com fatura/guia de arrecadação',
       '6' => 'Telefone com fatura/guia de arrecadação'
+    }
+  end
+
+  def localidades
+    @localidades = {
+      # Unidade sertão
+      '001' => 'Água Branca',
+      '016' => 'Canapi',
+      '025' => 'Delmiro Gouveia',
+      '031' => 'Pariconha',
+      '036' => 'Inhapi',
+      '054' => 'Mata Grande',
+      '062' => 'Olho D\'água do Casado',
+      '076' => 'Piranhas',
+      # Unidade Bacia Leiteira
+      '007' => 'Batalha',
+      '009' => 'Belo Monte',
+      '012' => 'Cacimbinhas',
+      '018' => 'Carneiros',
+      '026' => 'Dois Riachos',
+      '037' => 'Jacaré dos Homens',
+      '040' => 'Jaramataia',
+      '041' => 'Senador Rui Palmeira',
+      '048' => 'Major Izidoro',
+      '050' => 'Maravilha',
+      '058' => 'Monteiropoles',
+      '061' => 'Olho D\'água das Flores',
+      '064' => 'Olivença',
+      '065' => 'Ouro Branco',
+      '066' => 'Palestina',
+      '068' => 'Pão de Açucar',
+      '077' => 'Poço das Trincheiras',
+      '085' => 'Santana do Ipanema',
+      '089' => 'São José da Tapera',
+      # Unidade Agreste
+      '003' => 'Arapiraca',
+      '014' => 'Campo Alegre',
+      '015' => 'Campo Grande',
+      '020' => 'Coité do Noia',
+      '024' => 'Craibas',
+      '027' => 'Feira Grande',
+      '032' => 'Girau do Ponciano',
+      '034' => 'Igaci',
+      '035' => 'Igreja Nova',
+      '044' => 'Junqueiro',
+      '045' => 'Lagoa da Canoa',
+      '063' => 'Olho D\'água Grande',
+      '073' => 'Piaçabuçu',
+      '087' => 'São Braz',
+      '094' => 'São Sebastião',
+      '097' => 'Taquarana',
+      '098' => 'Traipu',
+      # Unidade Serrana
+      '002' => 'Anadia',
+      '017' => 'Capela',
+      '029' => 'Estrela de Alagoas',
+      '052' => 'Maribondo',
+      '053' => 'Mar Vermelho',
+      '057' => 'Minador do Negrão',
+      '067' => 'Palmeira dos Índios',
+      '071' => 'Paulo Jacinto',
+      '075' => 'Pindoba',
+      '081' => 'Quebrangulo',
+      # Unidade Leste
+      '006' => 'Barra De São Miguel',
+      '021' => 'Colonia Leopoldina',
+      '022' => 'Coqueiro Seco',
+      '030' => 'Flexeiras',
+      '033' => 'Ibateguara',
+      '038' => 'Jacuipe',
+      '039' => 'Japaratinga',
+      '042' => 'Joaquim Gomes',
+      '043' => 'Jundiá',
+      '049' => 'Maragogi',
+      '055' => 'Matriz de Camaragibe',
+      '056' => 'Messias',
+      '059' => 'Murici',
+      '060' => 'Novo Lino',
+      '069' => 'Paripueira',
+      '070' => 'Passo de Camaragibe',
+      '074' => 'Pilar',
+      '079' => 'Porto de Pedras',
+      '082' => 'Rio Largo',
+      '084' => 'Santa Luzia do Norte',
+      '095' => 'Satuba',
+      # Unidade Benedito Bentes
+      '471' => 'Maceió - Unidade Benedito Bentes',
+      '475' => 'Maceió - SANAMA Benedito Bentes',
+      # Unidade Farol
+      '472' => 'Maceió - Unidade Farol',
+      '474' => 'Maceió - SANEMA',
+      # Unidade jaraguá
+      '473' => 'Maceió  - Unidade Jaraguá'
     }
   end
 end
