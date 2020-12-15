f1e2641efb51723870724ebf6f747ad3a91ba05c
Randsson <randssondouglas@gmail.com>
2020-12-15 10:48:18 -0300

    generate devise for User with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+   
+  devise_for :users
   namespace :conta do
+    get 'cod_barra/index'
     get 'display/index'
     get 'search/index'
   end
   
   root to: "arquivos#index"
-  resources :invoice_items
   resources :arquivos
-  resources :invoices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
