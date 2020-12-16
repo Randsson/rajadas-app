1dbce1df0accefb05e783b008d765ff632dbfea7
Randsson <randssondouglas@gmail.com>
2020-12-16 14:20:44 -0300

    add condition for different layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,9 @@ module ApplicationHelper
     doc[1...-1]
   end
 
-
   def extrair_cod_barra_avulso(arquivo)
     path = ActiveStorage::Blob.service.path_for(arquivo.key)
-    doc = File.readlines(path)
+    File.readlines(path)
   end
 
   def verifica_duplicadas(arquivo)
@@ -37,19 +36,23 @@ module ApplicationHelper
   def extrair_cod_barra_filtrado(arquivo, termo, filtro)
     path = ActiveStorage::Blob.service.path_for(arquivo.key)
     doc = File.readlines(path)
-    lines = doc[1...-1]
     barra_filtrada = []
 
-    lines.each do |line|
+    doc.each do |line|
       case filtro # 1: NSR, 2: Tipo documento, 3: cod. localidade, 4: Forma arrecadacao
       when /[1\s]/ then barra_filtrada << line
       when '2' then  barra_filtrada << line if line[80].match(termo)
       when '3' then  barra_filtrada << line if line[37..39].match(termo)
       when '4' then  barra_filtrada << line if line[116].match(termo)
       when '5' then barra_filtrada << line if line[100..107].match(termo)
+      when '6' then barra_filtrada << line if datas_invalidas(line)
       end
     end
     barra_filtrada
+  end
+
+  def datas_invalidas(line)
+    line unless line[30..38].match(/\d{2}20\d{2}/)
   end
 
   def cod_barra_campos
@@ -58,7 +61,8 @@ module ApplicationHelper
       ['Tipo do documento', 2],
       ['Código da Localidade', 3],
       ['Forma de Arrecadação', 4],
-      ['NSR', 5]
+      ['NSR', 5],
+      ['Data Inválida', 6]
     ]
   end
 end
